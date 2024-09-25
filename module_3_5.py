aa951b2413c189c7b733e7cbf2e438f2dbaf2b1b
Dzhemal05 <136449640+Dzhemal05@users.noreply.github.com>
2024-09-25 12:50:11 +0200

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -5,8 +5,11 @@ def get_multiplied_digits(number):
     first = int(str_number[0])
     if first == 0:
         return get_multiplied_digits(str_number[1:])
-    return first * get_multiplied_digits(str_number[1:])
+    remaining_result = get_multiplied_digits(str_number[1:])
+    if remaining_result == 0:
+        return first
 
-result = get_multiplied_digits(40203)
+    return first * remaining_result
+
+result = get_multiplied_digits(420)
 print(result)
-
