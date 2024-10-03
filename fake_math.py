e8a46b61354b805b7c89112aa8f7e39f9de5d2f1
Dzhemal05 <136449640+Dzhemal05@users.noreply.github.com>
2024-10-03 10:06:43 +0200

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    if second != 0:
+        return round(first / second, 2)
+    else:
+        print('Ошибка!')
+
---
+++ true_math.py
@@ -0,0 +1,6 @@
+import math
+def divide(first, second):
+    if second != 0:
+        return round(first / second, 2)
+    else:
+        print(math.inf)
